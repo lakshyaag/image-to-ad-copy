ee57ff92d32981a1b6c5dd16c571c5bb3a636a62
lakshyaag <lakshya.agarwal@mail.mcgill.ca>
2023-12-21 20:22:08 -0500

    minor formatting

--- promo_pix/components/landing.tsx
+++ promo_pix/components/landing.tsx
@@ -55,7 +55,12 @@ export default function Landing() {
     <div className="flex flex-col items-center py-2 sm:flex sm:flex-1 sm:items-center sm:justify-center">
       <Toaster />
       <h1 className="mb-4 text-4xl font-bold">Welcome to {siteConfig.name}</h1>
-      <p className="mb-4 text-lg text-gray-600">{siteConfig.description}</p>
+      <p className="mb-2 text-lg">{siteConfig.description}</p>
+      <p className="text-md mb-4">
+        Simply enter the image URLs in the text box below and click{" "}
+        <span className="font-bold">&quot;Identify Products&quot;</span> to
+        begin
+      </p>
       <div className="m-4 grid w-full gap-2">
         <Label htmlFor="image-links">Enter image links</Label>
         <Textarea
